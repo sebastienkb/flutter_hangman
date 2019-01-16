cdeccf73914711625aa6923b28b0e727f79a8c56
Sébastien Kalb <skalb@altissia.com>
2019-01-16 15:52:58 +0100

    Add wrong letters section

--- flutter_hangman_app/lib/main.dart
+++ flutter_hangman_app/lib/main.dart
@@ -26,12 +26,24 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  Widget wrongLettersSection = Row(
+      children: [
+        Text(
+            '_ _ _ _ _ _'
+        )
+      ]
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+          title: Text(widget.title)
+      ),
+      body: Column(
+        children: <Widget>[
+          wrongLettersSection
+        ],
       ),
     );
   }
