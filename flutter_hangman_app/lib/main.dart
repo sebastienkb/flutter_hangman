a8a7ef643c57ff18d520e3b8edb71c7c1d00480c
twatillon <twatillon@altissia.com>
2019-01-17 07:18:56 +0100

    try some stuff

--- flutter_hangman_app/lib/main.dart
+++ flutter_hangman_app/lib/main.dart
@@ -25,27 +25,50 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   Widget imageSection = Row(children: [Image.asset('assets/Step0.png')]);
 
   Widget wrongLettersSection = Row(children: [Text('A B C')]);
 
-  Text answerSection = Text('_ _ _ _ _ _');
-
   @override
   Widget build(BuildContext context) {
-    Widget userInput = TextField(
-      decoration: InputDecoration(
-          border: InputBorder.none,
-          hintText: 'Please enter a letter'
-      ),
+    var word = "Toto";
+    var answerController = new TextEditingController();
+    var userInputController = new TextEditingController();
+
+    TextField answerSection = TextField(
+      controller: answerController,
     );
 
-    RaisedButton button = new RaisedButton(
-      child: new Text('Play!'),
-      color: Colors.blueAccent,
-      onPressed: toto(),
+    TextField userInput = TextField(
+      decoration: InputDecoration(
+          border: InputBorder.none, hintText: 'Please enter a letter'),
+      controller: userInputController,
     );
+
+    for (var i = 0; i < word.length; i++) {
+      answerController.text += "_ ";
+    }
+
+    void checkLetter(String letter) {
+      var indexes = List<int>();
+      for(var i = 0; i < word.length; i++) {
+        if(word[i] == letter) {
+          indexes.add(i);
+        }
+      }
+      if(indexes.isEmpty) {
+        //change picture and add letter to wrong letter section
+      } else {
+        //
+      }
+    }
+    RaisedButton button = new RaisedButton(
+        child: new Text('Play!'),
+        color: Colors.blueAccent,
+        onPressed: () {
+          answerController.text += userInputController.text;
+          userInputController.text = "";
+        });
 
     return Scaffold(
       appBar: AppBar(title: Text(widget.title)),
@@ -59,14 +82,5 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
     );
-  }
-
-  updateAnswer() {
-
-    answerSection = Text('potato');
-  }
-  toto() {
-
-    answerSection = Text('TOTO');
   }
 }
