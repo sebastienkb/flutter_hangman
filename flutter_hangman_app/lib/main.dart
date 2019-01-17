ec66add1b2aa9daa7ca9e6e1684a1a30095435bf
Sébastien Kalb <skalb@altissia.com>
2019-01-17 10:58:23 +0100

    Game Engine

--- flutter_hangman_app/lib/main.dart
+++ flutter_hangman_app/lib/main.dart
@@ -24,19 +24,70 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  Widget imageSection = Row(children: [Image.asset('assets/Step0.png')]);
+class GameEngine {
 
-  Widget wrongLettersSection = Row(children: [Text('A B C')]);
+  int attempts;
+  int allowedAttempts;
+  String answer;
+  String wrongAnswers;
+  String currentAnswer;
+
+  GameEngine(String answer, int allowedAttempts) {
+    this.attempts = 0;
+    this.allowedAttempts = allowedAttempts;
+    this.answer = answer.toUpperCase();
+    this.currentAnswer = answer.replaceAll(new RegExp(r'\w'), "_"); // _____
+    print(currentAnswer);
+    wrongAnswers = "";
+  }
+
+  giveAnswer(String letter) {
+    letter = letter.toUpperCase();
+    var foundCorrectLetter = false;
+    for(var i = 0; i < answer.length; i++) {
+      if (answer[i] == letter) {
+        currentAnswer = currentAnswer.replaceRange(i, i + 1, letter); // shit ?
+        foundCorrectLetter = true;
+      }
+    }
+
+    if (!foundCorrectLetter) {
+      attempts++;
+      this.wrongAnswers += letter;
+    }
+  }
+
+  String imageName() {
+    return "assets/Step" + attempts.toString() + ".png";
+  }
+
+  String prettyString(String s) {
+    String tmp = "";
+    for (var letter in s.split('')) {
+      tmp += letter + " ";
+    }
+    return tmp;
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  GameEngine gameEngine = GameEngine("TESLA", 7);
+//  Widget imageSection = Row(children: [Image.asset('assets/Step0.png')]);
+  Image imageSection = Image.asset('assets/Step0.png');
 
   @override
   Widget build(BuildContext context) {
-    var word = "Toto";
     var answerController = new TextEditingController();
+    var wrongAnswersController = new TextEditingController();
     var userInputController = new TextEditingController();
 
     TextField answerSection = TextField(
-      controller: answerController,
+      controller: answerController
+    );
+
+    TextField wrongAnswersSection = TextField(
+      controller: wrongAnswersController
     );
 
     TextField userInput = TextField(
@@ -45,28 +96,19 @@ class _MyHomePageState extends State<MyHomePage> {
       controller: userInputController,
     );
 
-    for (var i = 0; i < word.length; i++) {
+    for (var i = 0; i < gameEngine.answer.length; i++) {
       answerController.text += "_ ";
     }
 
-    void checkLetter(String letter) {
-      var indexes = List<int>();
-      for(var i = 0; i < word.length; i++) {
-        if(word[i] == letter) {
-          indexes.add(i);
-        }
-      }
-      if(indexes.isEmpty) {
-        //change picture and add letter to wrong letter section
-      } else {
-        //
-      }
-    }
     RaisedButton button = new RaisedButton(
         child: new Text('Play!'),
         color: Colors.blueAccent,
         onPressed: () {
-          answerController.text += userInputController.text;
+          gameEngine.giveAnswer(userInputController.text);
+          answerController.text = gameEngine.prettyString(gameEngine.currentAnswer);
+          wrongAnswersController.text = gameEngine.prettyString(gameEngine.wrongAnswers);
+          print(gameEngine.imageName());
+          imageSection = Image.asset(gameEngine.imageName());
           userInputController.text = "";
         });
 
@@ -75,7 +117,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Column(
         children: <Widget>[
           imageSection,
-          wrongLettersSection,
+          wrongAnswersSection,
           answerSection,
           userInput,
           button
